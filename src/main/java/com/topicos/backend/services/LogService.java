548ea92c7b7871dbc10dacf1f78b49bd7ad51665
loreferran <lorena.ferrando@mercadolibre.com>
2021-11-25 17:52:05 -0300

    endpoint logs

--- src/main/java/com/topicos/backend/services/LogService.java
+++ src/main/java/com/topicos/backend/services/LogService.java
@@ -4,6 +4,8 @@ import com.topicos.backend.dto.LogDTO;
 import com.topicos.backend.persistence.model.Log;
 import com.topicos.backend.persistence.repository.LogRepository;
 import com.topicos.backend.utils.Mappers;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,21 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class LogService {
 
-    private final LogRepository logRepository;
+  private final LogRepository logRepository;
 
-    public LogDTO addLog(LogDTO log) {
-        Log newLog = this.logRepository.save(Mappers.buildLog(log));
-        return Mappers.buildLogDTO(newLog);
-    }
+  public LogDTO addLog(LogDTO log) {
+    Log newLog = this.logRepository.save(Mappers.buildLog(log));
+    return Mappers.buildLogDTO(newLog);
+  }
 
 
+  public List<LogDTO> getAllLogs() {
+    List<Log> listLogs = this.logRepository.findAll();
+
+    return listLogs
+        .stream()
+        .map(Mappers::buildLogDTO)
+        .collect(Collectors.toList());
+  }
 
 }
